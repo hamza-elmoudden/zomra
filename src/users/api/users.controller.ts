48a72278b8623ad9ce73e473c80f17410a5355c6
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-10 18:30:08 +0100

    add find by id

--- src/users/api/users.controller.ts
+++ src/users/api/users.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req } from "@nestjs/common";
+import { CommandBus, QueryBus } from "@nestjs/cqrs";
+import { findUserByIdImpl } from "../application/queries/impl/find-user-byId.impl";
+import { User } from "../domain/entities/user.entity";
 
 
 
 
 @Controller('Users')
-export class UsersController{}
+export class UsersController{
+    constructor(
+        private readonly commandBus:CommandBus,
+        private readonly queryBus:QueryBus
+    ){}
+
+
+    @Get('me')
+    async findUserById(@Req() req:any):Promise<User>{
+
+
+        const user = await this.queryBus.execute(new findUserByIdImpl(req.user.id))
+
+        
+
+        return user
+    }
+}
