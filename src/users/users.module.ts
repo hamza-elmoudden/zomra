79e8f93f919d7b4fa8b1b3c6961ac591edc69e10
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-12 19:23:39 +0100

    change module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { UsersController } from './api/users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ID_USER_REPOSITORY } from './domain/repositories/user.repository';
 import { UserInfrastructure } from './infrastructure/user.infrastructure';
 import { findUserByIdHandler } from './application/queries/handler/find-user-byId.handler';
 import { CompleteUserHandler } from './application/commands/handler/complete-user.handler';
+import { UsersService } from './users.service';
 
 @Module({
-    controllers:[UsersController],
-    imports:[PrismaModule],
-    providers:[
+    controllers: [UsersController],
+    imports: [PrismaModule, CqrsModule],
+    providers: [
         {
-            provide:ID_USER_REPOSITORY,
-            useClass:UserInfrastructure
+            provide: ID_USER_REPOSITORY,
+            useClass: UserInfrastructure,
         },
         findUserByIdHandler,
         CompleteUserHandler,
-    ]
+        UsersService,
+    ],
+    exports: [UsersService],
 })
 export class UsersModule {}
