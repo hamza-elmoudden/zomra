0a9f003478ff38b9341cf8dcfc05753334650d3e
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-14 19:42:10 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
-
+ 
 
 @Module({
   imports: [
