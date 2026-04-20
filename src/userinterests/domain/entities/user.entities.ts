bb402b0bb3a5ae5a48c0b24043dd63570b7edf88
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-20 21:39:08 +0100

    module

--- src/userinterests/domain/entities/user.entities.ts
+++ src/userinterests/domain/entities/user.entities.ts
@@ -1,11 +1,12 @@
 
 
-
+export const USER_INTERESTS = "USER_INTERESTS"
 
 export class UserInterests{
     constructor(
+        public id:string,
         public user_id :string,
         public interest_id:string,
-        public DateTime:Date,
+        public DateTime: Date,
     ){}
 }
