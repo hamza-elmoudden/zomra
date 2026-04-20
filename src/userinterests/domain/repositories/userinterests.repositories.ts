0ad0c62e2ea97d246ba9b6f0e6d15dc2d45bed66
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-20 21:40:10 +0100

    change type to number

--- src/userinterests/domain/repositories/userinterests.repositories.ts
+++ src/userinterests/domain/repositories/userinterests.repositories.ts
@@ -1,9 +1,10 @@
+import { UserInterests } from "../entities/user.entities";
 
 
 
-export  abstract class UserInterests {
+export  abstract class UserInterestsRepositories {
     abstract getOneUserInterest(id:string):Promise<UserInterests | null>
     abstract getAllUserInterests(usrId:string):Promise<UserInterests[] | []>
-    abstract createUserInterest(userId:string,interest_id:string):Promise<UserInterests>
+    abstract createUserInterest(userId:string,interest_id:number):Promise<UserInterests>
     abstract deleteUserInterest(id:string):Promise<boolean>
 }
