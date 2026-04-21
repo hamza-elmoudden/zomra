2a2078d9d6becb1ec02ea8fa48df7b737453e19b
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-21 18:19:58 +0100

    add interests entities

---
+++ src/interests/domain/entities/interests.entities.ts
@@ -0,0 +1,10 @@
+
+
+export class Interests{
+    constructor(
+        public  id:string,
+        public readonly name:string,
+        public readonly icon?:string,
+        public readonly color_hex?:string
+    ){}
+}
