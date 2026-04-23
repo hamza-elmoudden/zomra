d839e56653f8648ad658b61e5a4c51c4a52e29b3
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-04-23 22:02:16 +0100

    full the conroller

--- src/interests/api/interests.controller.ts
+++ src/interests/api/interests.controller.ts
@@ -1,7 +1,51 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
+import { CommandBus, QueryBus } from "@nestjs/cqrs";
+import { GetAllInterestsImpl } from "../application/queries/impl/getall.interests.impl";
+import { CreateDto } from "./dto/create.dto";
+import { Roles } from "src/auth/decorators/decorators";
+import { AuthGuard } from "@nestjs/passport";
+import { CreateInterestsImpl } from "../application/commands/impl/create.interests.impl";
+import { DeleteInterestsImpl } from "../application/queries/impl/delete.interests.impl";
+import { GetInterestByIdImpl } from "../application/queries/impl/get.interestsByid.impl";
 
 
 @Controller('interests')
 export class InterestsController{
-    constructor(){}
+    constructor(
+        private readonly commandBus:CommandBus,
+        private readonly queryBus:QueryBus
+    ){}
+
+
+
+    @Get('all')
+    async getallInterests(){
+        return await this.queryBus.execute(new GetAllInterestsImpl())
+    }
+
+
+    @Get(':id')
+    @UseGuards(AuthGuard('jwt'))
+    async getById(@Param('id') id:number){
+        return await this.queryBus.execute(new GetInterestByIdImpl(id))
+    }
+
+    @Post()
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('admin')
+    async create(@Body() data:CreateDto){
+        return await this.commandBus.execute(new CreateInterestsImpl(data.name,data.icon,data.color_hex))
+    }
+
+
+    @Delete(':id')
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('admin')
+    async delete(@Param("id") id:number,@Req() req:any){
+
+        return await this.commandBus.execute(new DeleteInterestsImpl(req.user.id,id))
+
+    }
+
+
 }
