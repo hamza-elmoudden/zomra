f6dc585e61f5b637296ba26945c10c188a25a61d
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-05-02 21:44:42 +0100

    import and export

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { EVENTS_KAY } from './domain/repositories/events.repositories';
 import { EventsInfrastructure } from './infrastructure/events.infrastructure';
+import { CreateEventsHandler } from './application/commands/handler/create.events.handler';
+import { EventsController } from './api/events.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
     providers:[
         {   provide:EVENTS_KAY,
             useClass:EventsInfrastructure
-        }
-    ]
+        },
+        CreateEventsHandler
+    ],
+    controllers:[EventsController],
+    exports:[EVENTS_KAY],
+    imports:[UsersModule]
 })
 export class EventsModule {}
