2fc9e518644afec5ba02559e474b68a0ce005186
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-05-15 00:05:11 +0100

    domain repo and enities user

--- src/userinterests/domain/entities/user.entities.ts
+++ src/userinterests/domain/entities/user.entities.ts
@@ -1,12 +1,9 @@
-
-
 export const USER_INTERESTS = "USER_INTERESTS"
 
-export class UserInterests{
-    constructor(
-        public id:string,
-        public user_id :string,
-        public interest_id:string,
-        public DateTime: Date,
-    ){}
+export class UserInterests {
+  constructor(
+    public readonly user_id: string,
+    public readonly interest_id: number,
+    public readonly created_at?: Date,
+  ) {}
 }
--- src/userinterests/domain/repositories/userinterests.repositories.ts
+++ src/userinterests/domain/repositories/userinterests.repositories.ts
@@ -1,10 +1,8 @@
 import { UserInterests } from "../entities/user.entities";
 
-
-
-export  abstract class UserInterestsRepositories {
-    abstract getOneUserInterest(id:string):Promise<UserInterests | null>
-    abstract getAllUserInterests(usrId:string):Promise<UserInterests[] | []>
-    abstract createUserInterest(userId:string,interest_id:number):Promise<UserInterests>
-    abstract deleteUserInterest(id:string):Promise<boolean>
+export abstract class UserInterestsRepositories {
+    abstract getAllUserInterests(userId: string): Promise<UserInterests[]>
+    abstract getOneUserInterest(userId: string, interestId: number): Promise<UserInterests | null>
+    abstract createUserInterest(userId: string, interestId: number): Promise<UserInterests>
+    abstract deleteUserInterest(userId: string, interestId: number): Promise<void>
 }
