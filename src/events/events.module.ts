6888ff79c886af9fa5ced262763ebb29691a5241
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-05-14 10:36:32 +0100

    update module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,21 +1,48 @@
 import { Module } from '@nestjs/common';
 import { EVENTS_KAY } from './domain/repositories/events.repositories';
+import { EVENT_PARTICIPANT_KEY } from './domain/repositories/event-participant.repository';
 import { EventsInfrastructure } from './infrastructure/events.infrastructure';
+import { EventParticipantInfrastructure } from './infrastructure/event-participant.infrastructure';
 import { CreateEventsHandler } from './application/commands/handler/create.events.handler';
+import { UpdateEventHandler } from './application/commands/handler/update-event.handler';
+import { DeleteEventHandler } from './application/commands/handler/delete-event.handler';
+import { JoinEventHandler } from './application/commands/handler/join-event.handler';
+import { LeaveEventHandler } from './application/commands/handler/leave-event.handler';
+import { AcceptParticipantHandler } from './application/commands/handler/accept-participant.handler';
+import { RejectParticipantHandler } from './application/commands/handler/reject-participant.handler';
+import { GetEventByIdHandler } from './application/queries/handler/get-event-by-id.handler';
+import { ListEventsHandler } from './application/queries/handler/list-events.handler';
+import { GetNearbyEventsHandler } from './application/queries/handler/get-nearby-events.handler';
+import { GetEventParticipantsHandler } from './application/queries/handler/get-event-participants.handler';
 import { EventsController } from './api/events.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-    providers:[
-        {   provide:EVENTS_KAY,
-            useClass:EventsInfrastructure
+    providers: [
+        {
+            provide: EVENTS_KAY,
+            useClass: EventsInfrastructure,
         },
-        CreateEventsHandler
+        {
+            provide: EVENT_PARTICIPANT_KEY,
+            useClass: EventParticipantInfrastructure,
+        },
+        CreateEventsHandler,
+        UpdateEventHandler,
+        DeleteEventHandler,
+        JoinEventHandler,
+        LeaveEventHandler,
+        AcceptParticipantHandler,
+        RejectParticipantHandler,
+        GetEventByIdHandler,
+        ListEventsHandler,
+        GetNearbyEventsHandler,
+        GetEventParticipantsHandler,
     ],
-    controllers:[EventsController],
-    exports:[EVENTS_KAY],
-    imports:[UsersModule,PrismaModule,CqrsModule]
+    controllers: [EventsController],
+    exports: [EVENTS_KAY, EVENT_PARTICIPANT_KEY],
+    imports: [UsersModule, PrismaModule, CqrsModule],
 })
 export class EventsModule {}
