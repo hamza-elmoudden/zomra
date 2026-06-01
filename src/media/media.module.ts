e7ba26e91b978edd6442383d480cc77f9bb27197
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-06-01 19:42:44 +0100

    media moudle up

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,16 +1,29 @@
-import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { ID_MEDIA_REPOSITORY } from './domain/repositories/media.repository';
-import { MediaInfrastructure } from './infrastructure/media.infrastructure';
-import { StorageService } from './infrastructure/storage.service';
-import { UploadMediaHandler } from './application/commands/handler/upload-media.handler';
-import { DeleteMediaHandler } from './application/commands/handler/delete-media.handler';
-import { GetEventMediaHandler } from './application/queries/handler/get-event-media.handler';
-import { MediaController } from './api/media.controller';
+import { Module } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
+import { MulterModule } from "@nestjs/platform-express";
+import { memoryStorage } from "multer";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { EventsModule } from "src/events/events.module";
+import { ID_MEDIA_REPOSITORY } from "./domain/repositories/media.repository";
+import { MediaInfrastructure } from "./infrastructure/media.infrastructure";
+import { StorageService } from "./infrastructure/storage.service";
+import { UploadMediaHandler } from "./application/commands/handler/upload-media.handler";
+import { DeleteMediaHandler } from "./application/commands/handler/delete-media.handler";
+import { GetEventMediaHandler } from "./application/queries/handler/get-event-media.handler";
+import { MediaController } from "./api/media.controller";
 
 @Module({
-  imports: [PrismaModule, CqrsModule],
+  imports: [
+    PrismaModule,
+    CqrsModule,
+    EventsModule,
+    MulterModule.register({
+      storage: memoryStorage(),
+      limits: {
+        fileSize: 50 * 1024 * 1024,
+      },
+    }),
+  ],
   controllers: [MediaController],
   providers: [
     {
