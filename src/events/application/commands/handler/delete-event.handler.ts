46b6dcf0c7eb875aa791823032d60e46daf268d7
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-06-13 15:54:46 +0100

    update the delete to make him pass and block the delelte if the event pass and have users

--- src/events/application/commands/handler/delete-event.handler.ts
+++ src/events/application/commands/handler/delete-event.handler.ts
@@ -1,7 +1,8 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { DeleteEventImpl } from "../impl/delete-event.impl";
-import { Inject, NotFoundException, ForbiddenException, InternalServerErrorException } from "@nestjs/common";
+import { Inject, NotFoundException, ForbiddenException, BadRequestException, InternalServerErrorException } from "@nestjs/common";
 import { EVENTS_KAY, EventsRepositories } from "src/events/domain/repositories/events.repositories";
+import { EVENT_PARTICIPANT_KEY, EventParticipantRepository } from "src/events/domain/repositories/event-participant.repository";
 
 @CommandHandler(DeleteEventImpl)
 export class DeleteEventHandler implements ICommandHandler<DeleteEventImpl> {
@@ -9,6 +10,8 @@ export class DeleteEventHandler implements ICommandHandler<DeleteEventImpl> {
   constructor(
     @Inject(EVENTS_KAY)
     private readonly repo: EventsRepositories,
+    @Inject(EVENT_PARTICIPANT_KEY)
+    private readonly participantRepo: EventParticipantRepository,
   ) {}
 
   async execute(command: DeleteEventImpl): Promise<void> {
@@ -20,6 +23,14 @@ export class DeleteEventHandler implements ICommandHandler<DeleteEventImpl> {
 
     if (existing.host_id !== command.userId) {
       throw new ForbiddenException('Only the event host can delete this event')
+    }
+
+    const eventEnd = new Date(existing.starts_at.getTime() + existing.duration_minutes * 60000)
+    if (eventEnd < new Date()) {
+      const participantCount = await this.participantRepo.countByEventId(command.id)
+      if (participantCount > 0) {
+        throw new BadRequestException('Cannot delete an event that has ended and has participants')
+      }
     }
 
     try {
